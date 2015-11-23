0d7e3200c122f60d0ed816931bd6cc0d94de944d
Zhong Zhou <zhongzhou@cmu.edu>
2015-11-23 17:43:55 -0500

    Script for selecting given tags from dictionary

---
+++ select_tags.py
@@ -0,0 +1,25 @@
+import json
+import sys
+
+from collections import defaultdict
+
+dictPath = sys.argv[1]
+tagPath = sys.argv[2]
+outPath = sys.argv[3]
+
+def create_dict(f):
+	tag_dict = defaultdict(int)
+	for line in f:
+		row = line.rstrip('\n').split(' - ')
+		tag_dict[row[0]] = int(row[1])
+	return tag_dict
+
+def add_tags(f_in, f_out, tag_dict):
+	for line in f_in:
+		current = json.loads(line)
+		if current[0] in tag_dict:
+			f_out.write(line)
+
+tag_dict = create_dict(open(tagPath,'r'))
+add_tags(open(dictPath,'r'), open(outPath,'w'), tag_dict)
+print sum(tag_dict.values())
